8976fe9f5e12f9931c5116a37f4de68f96d363e4
vinayaka0305 <vav6473@gmail.com>
2023-08-09 13:34:25 +0530

    code

---
+++ JS/linking-script.Basic.js
@@ -0,0 +1,21 @@
+console.log("vinayaka AV")
+ 
+// Declartion
+var xyz;
+console.log(xyz);
+
+// Intialization
+
+xyz = "alphabets"
+console.log(xyz);
+
+
+// Declaration+Intialization
+
+var abc = 12;
+console.log(abc);
+
+// similar
+
+ num = 14;
+ console.log(num);
